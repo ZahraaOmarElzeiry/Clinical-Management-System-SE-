16ee67dcca90ec3ba6e3f21fc71dfd6bb99c48a5
Zahraa Omar <103336709+ZahraaOmarElzeiry@users.noreply.github.com>
2022-12-14 00:17:37 +0200

    Created the main project structure

---
+++ Clinical Management System (Software)/Clinical Management System (Software)/DoctorPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clinical_Management_System__Software_
+{
+    public partial class DoctorPanel : Form
+    {
+        public DoctorPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
